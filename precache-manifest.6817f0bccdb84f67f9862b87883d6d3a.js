8c999472cd7e21a8ee1e75b49fae732a234c47f5
Guan Da <luffychen0715@gmail.com>
2020-04-12 15:17:24 +0800

    Updates

---
+++ precache-manifest.6817f0bccdb84f67f9862b87883d6d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9735b754dcd265af96d443d022c2cefc",
+    "url": "/todo-list-react-redux/index.html"
+  },
+  {
+    "revision": "2047f1e63c4546d7f408",
+    "url": "/todo-list-react-redux/static/css/main.79e20959.chunk.css"
+  },
+  {
+    "revision": "de0f06c8665010a23236",
+    "url": "/todo-list-react-redux/static/js/2.68e5c6fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list-react-redux/static/js/2.68e5c6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2047f1e63c4546d7f408",
+    "url": "/todo-list-react-redux/static/js/main.0aa41ddb.chunk.js"
+  },
+  {
+    "revision": "72ef164a491301a2709d",
+    "url": "/todo-list-react-redux/static/js/runtime-main.56be84e4.js"
+  }
+]);
